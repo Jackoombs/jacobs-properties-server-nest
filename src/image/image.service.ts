deca308bd7388917506314515918fdad79593142
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-03 13:30:59 +0100

    Added image service to process and upload property image.

---
+++ src/image/image.service.ts
@@ -0,0 +1,65 @@
+import { Injectable } from '@nestjs/common';
+import sharp from 'sharp';
+import ImageKit from 'imagekit';
+import axios from 'axios';
+import { PropertyImageModel } from '@reapit/foundations-ts-definitions';
+
+@Injectable()
+export class ImageService {
+  private readonly imagekit = new ImageKit({
+    publicKey: 'public_4LM2PTPwrb1Le2NlI1aDbdce/A4=',
+    privateKey: process.env.IMAGEKIT_KEY,
+    urlEndpoint: 'https://ik.imagekit.io/k6joqq39e',
+  });
+
+  async processAllImages(images: PropertyImageModel[]) {
+    for (const [index, image] of images.entries()) {
+      if (index === 0) {
+        this.processImage(
+          image.url,
+          image.id + '-sm.webp',
+          image.propertyId,
+          450,
+          300,
+        );
+      }
+      this.processImage(
+        image.url,
+        image.id + '-lg.webp',
+        image.propertyId,
+        825,
+        550,
+      );
+    }
+  }
+
+  async processImage(
+    url: string,
+    fileName: string,
+    dirName: string,
+    width: number,
+    height: number,
+  ) {
+    const res = await axios.get(url, { responseType: 'stream' });
+    const imageData = await res.data;
+    const resizedData = await this.resizeImage(imageData, width, height);
+    this.uploadImage(resizedData, fileName, dirName);
+  }
+
+  async resizeImage(image: Buffer, width: number, height: number) {
+    return await sharp(image)
+      .resize(width, height)
+      .webp({ quality: 80 })
+      .toBuffer();
+  }
+
+  async uploadImage(image: Buffer, fileName: string, dirName: string) {
+    await this.imagekit.upload({
+      file: image,
+      fileName: fileName,
+      folder: dirName,
+      useUniqueFileName: false,
+      overwriteFile: false,
+    });
+  }
+}
