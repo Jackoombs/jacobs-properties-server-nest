5dbeccf4fef5c902c6308af16ab0a55a77a7ee5c
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-03 13:38:08 +0100

    App module file auto update.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ReapitService } from './reapit/reapit.service';
+import { ReapitController } from './reapit/reapit.controller';
+import { FileService } from './file/file.service';
+import { DataService } from './data/data.service';
+import { ImageService } from './image/image.service';
+import { EmailService } from './email/email.service';
+import { PropertyService } from './property/property.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ReapitController],
+  providers: [
+    AppService,
+    ReapitService,
+    FileService,
+    DataService,
+    ImageService,
+    EmailService,
+    PropertyService,
+  ],
 })
 export class AppModule {}
