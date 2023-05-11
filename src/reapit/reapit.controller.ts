f48867a26ae4bb9512eb330c90f9b5cbc9a3d1ee
Jack Coombs <jack.coombs@hotmail.co.uk>
2023-05-11 15:41:40 +0100

    Commented out reapit endpoint.

--- src/reapit/reapit.controller.ts
+++ src/reapit/reapit.controller.ts
@@ -17,49 +17,49 @@ export class ReapitController {
     return 'Hello World!';
   }
 
-  @Post()
-  async updateProperty(@Body() payload: ReapitWebhook) {
-    const properties = await this.propertyService.readProperties();
-    const property = await this.reapitService.fetchProperty(payload.entityId);
-    const formattedProperty = this.propertyService.formatProperty(property);
-    let updatedProperties: FormattedProperty[];
+  // @Post()
+  // async updateProperty(@Body() payload: ReapitWebhook) {
+  //   const properties = await this.propertyService.readProperties();
+  //   const property = await this.reapitService.fetchProperty(payload.entityId);
+  //   const formattedProperty = this.propertyService.formatProperty(property);
+  //   let updatedProperties: FormattedProperty[];
 
-    const allowedStatus: AllowedStatus[] = [
-      'forSale',
-      'underOffer',
-      'exchanged',
-      'reserved',
-      'completed',
-      'soldExternally',
-      'toLet',
-      'underOffer',
-      'arrangingTenancy',
-      'tenancyCurrent',
-      'tenancyFinished',
-      'sold',
-    ];
+  //   const allowedStatus: AllowedStatus[] = [
+  //     'forSale',
+  //     'underOffer',
+  //     'exchanged',
+  //     'reserved',
+  //     'completed',
+  //     'soldExternally',
+  //     'toLet',
+  //     'underOffer',
+  //     'arrangingTenancy',
+  //     'tenancyCurrent',
+  //     'tenancyFinished',
+  //     'sold',
+  //   ];
 
-    if (allowedStatus.includes(formattedProperty.status as AllowedStatus)) {
-      if (payload.topicId === 'properties.modified') {
-        updatedProperties = this.propertyService.updateProperty(
-          formattedProperty,
-          properties,
-        );
-      } else if (payload.topicId === 'properties.created') {
-        updatedProperties = this.propertyService.addProperty(
-          formattedProperty,
-          properties,
-        );
-      }
-      await this.imageService.processAllImages(formattedProperty.images);
-    } else {
-      updatedProperties = this.propertyService.removeProperty(
-        formattedProperty,
-        properties,
-      );
-    }
+  //   if (allowedStatus.includes(formattedProperty.status as AllowedStatus)) {
+  //     if (payload.topicId === 'properties.modified') {
+  //       updatedProperties = this.propertyService.updateProperty(
+  //         formattedProperty,
+  //         properties,
+  //       );
+  //     } else if (payload.topicId === 'properties.created') {
+  //       updatedProperties = this.propertyService.addProperty(
+  //         formattedProperty,
+  //         properties,
+  //       );
+  //     }
+  //     await this.imageService.processAllImages(formattedProperty.images);
+  //   } else {
+  //     updatedProperties = this.propertyService.removeProperty(
+  //       formattedProperty,
+  //       properties,
+  //     );
+  //   }
 
-    await this.propertyService.writeProperties(updatedProperties);
-    await this.reapitService.deployFrontend();
-  }
+  //   await this.propertyService.writeProperties(updatedProperties);
+  //   await this.reapitService.deployFrontend();
+  // }
 }
